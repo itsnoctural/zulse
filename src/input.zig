e98ad7dc0736c22febda2c0b02ac4b986f5715a7
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2026-06-11 17:30:24 +0300

    style: allign fn types with kernel

--- src/input.zig
+++ src/input.zig
@@ -7,15 +7,17 @@ pub const InputEvent = extern struct {
     value: i32,
 };
 
-fn ioc(comptime dir: u32, comptime typ: u32, comptime nr: u32, comptime size: u32) u32 {
+// https://github.com/torvalds/linux/blob/v7.0/rust/kernel/ioctl.rs#L13
+inline fn ioc(dir: u32, typ: u32, nr: u32, size: usize) u32 {
     return (dir << 30) | (typ << 8) | (nr << 0) | (size << 16);
 }
 
-pub fn eviocgbit(comptime ev: u32, comptime len: u32) u32 {
+pub inline fn eviocgbit(ev: u32, len: usize) u32 {
     return ioc(2, 'E', 0x20 + ev, len);
 }
 
-pub fn test_bit(nr: u32, addr: []const u64) bool {
+// https://github.com/torvalds/linux/blob/v7.0/tools/include/asm-generic/bitops/non-atomic.h#L110
+pub inline fn test_bit(nr: u64, addr: []const u64) bool {
     const shift: u6 = @intCast(nr & (64 - 1));
     return 1 & (addr[nr / 64] >> shift) != 0;
 }
